ab0b87e20a3d3c6ff7a67d0813ce167ef2ccc1c1
Azner01 <azneraparicio@hotmail.com>
2024-06-10 22:46:55 -0500

    Azner (V1.4)
    Implementación del mapa de figma al sitio web

--- src/components/MapaDeUTP.jsx
+++ src/components/MapaDeUTP.jsx
@@ -1,11 +1,16 @@
 export default function MapaDeUTP() {
+
 	return (
 		// Esta Imagen esta puesta por mientras
 		//hay q cambiarlo por un mapa adecuado
-		<img
+		/*<*/
+			/*img
             className='w-1/2 mx-auto mt-6 h-1/2'
 			src='https://scontent.fpac1-4.fna.fbcdn.net/v/t39.30808-6/293020926_5785629408115106_6240995517341201380_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=NR3jJg09pUwQ7kNvgGpwryN&_nc_ht=scontent.fpac1-4.fna&oh=00_AYCxnfpbBtBp4vePmuZmpIVHVjigKH3n9srUS3RYGY4QgQ&oe=66699639'
-			alt=''
-		/>
+			alt=''*/
+			<div className="w-1/2 mx-auto mt-6 h-1/2">
+				<div dangerouslySetInnerHTML={{ __html: `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FE4dZaxpZGgxDAAuYTI2smL%2FUniFloorMap%3Fnode-id%3D0-1%26t%3Dt6Sp3qr8WSCpuLU4-1" allowfullscreen></iframe>` }} />
+			</div>
+		/*/>*/
 	)
 }
